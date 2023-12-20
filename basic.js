8eca120a147afda878e5b6ba6dddef5c64b83542
SAHA-YJB <pkomom123@naver.com>
2023-12-20 11:18:40 +0900

    라우팅 분기처리 해보기

--- basic.js
+++ basic.js
@@ -12,17 +12,30 @@ const port = 4000;
 // });
 
 //제이슨 보내주기
+//라우팅 처리(분기처리)
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({
-      message: 'Hello World',
-      code: 200,
-      b: 1,
-      c: 2,
-      d: 3,
-    })
-  );
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        message: 'Hello World',
+        code: 200,
+        b: 1,
+        c: 2,
+        d: 3,
+      })
+    );
+  } else if (req.url === '/about') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<h1>About Hello</h1>');
+    res.write('</body>');
+    res.write('</html>');
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(port, () => {
