31af77a9ba7a0e819246dae05aedd777e0a108d4
SAHA-YJB <pkomom123@naver.com>
2023-12-20 11:32:25 +0900

    post 요청해보기

--- basic.js
+++ basic.js
@@ -1,6 +1,13 @@
 const http = require('http');
 
 const port = 4000;
+const targetObj = {
+  message: 'Hello World',
+  code: 200,
+  b: 1,
+  c: 2,
+  d: 3,
+};
 //텍스트 보내주기
 // const server = http.createServer((req, res) => {
 //   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -13,28 +20,30 @@ const port = 4000;
 
 //제이슨 보내주기
 //라우팅 처리(분기처리)
+//포스트메소드 추가해보기
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(
-      JSON.stringify({
-        message: 'Hello World',
-        code: 200,
-        b: 1,
-        c: 2,
-        d: 3,
-      })
-    );
-  } else if (req.url === '/about') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<body>');
-    res.write('<h1>About Hello</h1>');
-    res.write('</body>');
-    res.write('</html>');
+  if (req.method === 'POST' && req.url === '/') {
+    req.on('data', (data) => {
+      console.log('data', data);
+      const stringfiedData = data.toString();
+      console.log('', stringfiedData);
+      Object.assign(targetObj, JSON.parse(stringfiedData));
+    });
   } else {
-    res.statusCode = 404;
-    res.end();
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(targetObj));
+    } else if (req.url === '/about') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<body>');
+      res.write('<h1>About Hello</h1>');
+      res.write('</body>');
+      res.write('</html>');
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
   }
 });
 
